17ee5c50ca31a3a6cd9dc8dcdb32cd40cc09a087
zeyangxu <zx638@nyu.edu>
2018-10-05 22:51:04 -0400

    rewrite to SPA and  add pagination and   nav bar highlight

--- js/index.js
+++ js/index.js
@@ -6,25 +6,25 @@ let query_o = 'mr';
 let searchMode = false;
 let scroll = 0;
 let url = end_point + '0?c=21&o=mr&limit=10';
+let total_page = 0;
 
 $(document).ready(() => {
   getMovies();
-  $('#moreButton').on('click', () => {
-    console.log('button click, more is:' + hasMore);
-    if (hasMore) {
-      page++;
-      if (searchMode) {
-        let input = $('.form-inline input').val();
-        url = `https://api.avgle.com/v1/search/${input}/${page}?limit=5`;
-      } else {
-        url = end_point + page + `?c=21&limit=5&o=${query_o}`;
-      }
-      getMovies();
-      console.log();
-    } else {
-      alert('No more movie');
-    }
-  });
+  // $('#moreButton').on('click', () => {
+  //   console.log('button click, more is:' + hasMore);
+  //   if (hasMore) {
+  //     page++;
+  //     if (searchMode) {
+  //       let input = $('.form-inline input').val();
+  //       url = `https://api.avgle.com/v1/search/${input}/${page}?limit=5`;
+  //     } else {
+  //       url = end_point + page + `?c=21&limit=5&o=${query_o}`;
+  //     }
+  //     getMovies();
+  //   } else {
+  //     alert('No more movie');
+  //   }
+  // });
 
   $('.navbar-brand').click(() => {
     $('.movie-group').show();
@@ -34,6 +34,7 @@ $(document).ready(() => {
   });
 
   $('#mr').click(() => {
+    console.log('mr clicked!');
     $('.movie-group').show();
     $('#moreButton').show();
     $('.movie-info').empty();
@@ -64,7 +65,20 @@ $(document).ready(() => {
     url = `https://api.avgle.com/v1/search/${input}/${page}?limit=10`;
     refreshMovies();
   });
+
+  $('body').on('click', 'a.page-link', e => {
+    console.log('a click!');
+    e.preventDefault();
+  });
 });
+
+function gotoPage(p) {
+  page = p;
+  $('.movie-group').empty();
+  url = end_point + page + `?c=21&limit=10&o=${query_o}`;
+  getMovies();
+  return false;
+}
 
 function changeQuery(type, object) {
   query_o = type;
@@ -78,6 +92,7 @@ function movieSelected(id) {
   // window.location = 'movie.html';
   scroll = $(window).scrollTop();
 
+  $('nav.pageNav').hide();
   $('.movie-group').hide();
   $('#moreButton').hide();
   getVideoPage(id);
@@ -85,12 +100,21 @@ function movieSelected(id) {
 }
 
 function getVideoPage(id) {
-  console.log('info page, id: ' + id);
   axios.get(search_end_point + id).then(res => {
-    console.log(res);
-    let movie = res.data.response.video;
-    console.log(movie);
-    let output = `
+    if (res.data.success == false) {
+      let output = `
+        <button class="btn btn-primary" onclick="goBack()">
+          Go back
+        </button>
+        <div class="alert alert-danger mt-4" role="alert">
+          ${res.data.response.error_message}
+        </div>
+      `;
+      $('.movie-info').append(output);
+    } else {
+      let movie = res.data.response.video;
+      console.log(movie);
+      let output = `
     <button class="btn btn-primary" onclick="goBack()">
       Go back
     </button>
@@ -106,7 +130,8 @@ function getVideoPage(id) {
       referrerpolicy="no-referrer-when-downgrade"
       src="${movie.embedded_url}"></iframe>
     `;
-    $('.movie-info').append(output);
+      $('.movie-info').append(output);
+    }
   });
 }
 
@@ -115,6 +140,7 @@ function goBack() {
   $('#moreButton').show();
   $('.movie-info').empty();
   $('html').scrollTop(scroll);
+  $('nav.pageNav').show();
 }
 
 function textCopy() {
@@ -145,28 +171,132 @@ function getMovies() {
     .then(res => {
       console.log(res.data.response);
       let movies = res.data.response.videos;
+      total_page = Math.floor(res.data.response.total_videos / 10) + 1;
+      console.log(total_page);
       $.each(movies, (index, movie) => {
         let output = `
-            <div class="movie-card shadow bg-light">
-              <img src="${movie.preview_url}" onclick="movieSelected(${
+          <div class="movie-card shadow bg-light">
+            <img src="${movie.preview_url}" onclick="movieSelected(${
           movie.vid
         })">
-              <video class="preview-video" loop autoplay onclick="movieSelected(${
-                movie.vid
-              })">
-                <source src="${movie.preview_video_url}" type="video/mp4">
-              </video>
-              <a href="#" onclick="movieSelected(${movie.vid})">${
-          movie.title
-        }</a>
-            </div>
+            <video class="preview-video" loop autoplay onclick="movieSelected(${
+              movie.vid
+            })">
+              <source src="${movie.preview_video_url}" type="video/mp4">
+            </video>
+            <a href="#" onclick="movieSelected(${movie.vid})">${movie.title}</a>
+          </div>
         `;
         $('.movie-group').append(output);
         // $('video')[index].play();
       });
       hasMore = res.data.response.has_more;
+      renderPagination();
+      getActivePageNum();
+      getActiveTab();
     })
     .catch(error => {
       console.log(error);
     });
+}
+
+function renderPagination() {
+  if (page > 1) {
+    $('.pagination').html(
+      `
+      <li class="page-item">
+      <a class="page-link" href="#" id="prevButton" onclick="handlePrevButton()">
+        <span aria-hidden="true">&laquo;</span>
+        <span class="sr-only">Previous</span>
+      </a>
+    </li>
+        <li class="page-item">
+          <a class="page-link" 
+          href="#" 
+          onclick="gotoPage(${page - 1})">
+            ${page}
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#" onclick="gotoPage(${page})">
+            ${page + 1}
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#" onclick="gotoPage(${page + 1})">
+            ${page + 2}
+          </a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">...</a></li>
+        <li class="page-item"><a class="page-link" href="#" onclick="gotoPage(${total_page})">${total_page}</a></li>
+        <li class="page-item">
+            <a class="page-link" href="#" id="nextButton" onclick="handleNextButton()">
+              <span aria-hidden="true">&raquo;</span>
+              <span class="sr-only">Next</span>
+            </a>
+          </li>
+      `
+    );
+  } else {
+    $('.pagination').html(
+      `
+      <li class="page-item">
+      <a class="page-link" href="#" id="prevButton" onclick="handlePrevButton()">
+        <span aria-hidden="true">&laquo;</span>
+        <span class="sr-only">Previous</span>
+      </a>
+    </li>
+        <li class="page-item"><a class="page-link" href="#" onclick="gotoPage(0)">1</a></li>
+        <li class="page-item"><a class="page-link" href="#" onclick="gotoPage(1)">2</a></li>
+        <li class="page-item"><a class="page-link" href="#" onclick="gotoPage(2)">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">...</a></li>
+        <li class="page-item"><a class="page-link" href="#" onclick="gotoPage(${total_page})">${total_page}</a></li>
+        <li class="page-item">
+            <a class="page-link" href="#" id="nextButton" onclick="handleNextButton()">
+              <span aria-hidden="true">&raquo;</span>
+              <span class="sr-only">Next</span>
+            </a>
+          </li>
+      `
+    );
+  }
+}
+
+function getActiveTab() {
+  $('li.nav-item.active').toggleClass('active');
+  $(`.nav-link[id*="${query_o}"]`)
+    .parent()
+    .toggleClass('active');
+}
+
+function getActivePageNum() {
+  $(`.page-link:contains("${page + 1}")`)
+    .parent()
+    .toggleClass('active');
+}
+
+function handleNextButton() {
+  page++;
+  if (searchMode) {
+    let input = $('.form-inline input').val();
+    url = `https://api.avgle.com/v1/search/${input}/${page}?limit=5`;
+  } else {
+    url = end_point + page + `?c=21&limit=5&o=${query_o}`;
+  }
+  $('.movie-group').empty();
+  getMovies();
+}
+
+function handlePrevButton() {
+  if (page > 1) {
+    page--;
+  }
+  if (searchMode) {
+    let input = $('.form-inline input').val();
+    url = `https://api.avgle.com/v1/search/${input}/${page}?limit=5`;
+  } else {
+    url = end_point + page + `?c=21&limit=5&o=${query_o}`;
+  }
+  $('.movie-group').empty();
+  getMovies();
 }
